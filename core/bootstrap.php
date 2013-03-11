2b392a8fb7274606651490906cf5f975c0807005
karakuri <s@umecha.biz>
2013-03-11 13:32:42 +0900

    デフォルトでエラーを例外に変換

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,5 +1,11 @@
 <?php
 
+// デフォルトでエラーを例外に変換
+function exception_error_handler($errno, $errstr, $errfile, $errline ) {
+    throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
+}
+set_error_handler("exception_error_handler");
+
 include 'paths.php';
 include APP_PATH . DS . 'env.php';
 include APP_PATH . DS . 'vendor' . DS . 'autoload.php';
